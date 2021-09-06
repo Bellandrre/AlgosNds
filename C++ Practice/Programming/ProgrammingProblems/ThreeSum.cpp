9f7a50527170a8236b0097c0eb2f0b6040426cef
Karthik Kolathumani <karthik_kr@hotmail.com>
2021-09-05 21:42:53 -0700

    handle edge cases in threeSum

--- C++ Practice/Programming/ProgrammingProblems/ThreeSum.cpp
+++ C++ Practice/Programming/ProgrammingProblems/ThreeSum.cpp
@@ -15,27 +15,29 @@ vector<vector<int>> computeThreeSum(int arr[], int arr_size){
     int size = arr_size;
 
     int target = 0;
-    sort(arr,  arr + size);
+    if(size > 2) {
+        sort(arr, arr + size);
 
-    for(int i = 0 ; i < size - 3; i++){
-        if(i == 0 || arr[i] > arr[i-1]){ // To make sure we are not considering duplicate elements
-            int start = i+1;
-            int end = size - 1;
-            while(start <  end){
-                int currSum = arr[i] + arr[start] + arr[end];
-                if(currSum == target){
-                    vector<int> currResultSet = {arr[i], arr[start], arr[end]};
-                    resultSet.push_back(currResultSet);
-                }
-                if(currSum < 0){ // then move sum is too low move start pointer
-                    int currStart = start;
-                    while(arr[currStart] == arr[start] && start < end){
-                        start++;
+        for (int i = 0; i <= size - 3; i++) {
+            if (i == 0 || arr[i] > arr[i - 1]) { // To make sure we are not considering duplicate elements
+                int start = i + 1;
+                int end = size - 1;
+                while (start < end) {
+                    int currSum = arr[i] + arr[start] + arr[end];
+                    if (currSum == target) {
+                        vector<int> currResultSet = {arr[i], arr[start], arr[end]};
+                        resultSet.push_back(currResultSet);
                     }
-                }else{ // Sum is too big
-                    int currEnd = end;
-                    while(arr[currEnd] == arr[end] && start < end){
-                        end--;
+                    if (currSum < 0) { // then move sum is too low move start pointer
+                        int currStart = start;
+                        while (arr[currStart] == arr[start] && start < end) {
+                            start++;
+                        }
+                    } else { // Sum is too big
+                        int currEnd = end;
+                        while (arr[currEnd] == arr[end] && start < end) {
+                            end--;
+                        }
                     }
                 }
             }
@@ -46,7 +48,7 @@ vector<vector<int>> computeThreeSum(int arr[], int arr_size){
 
 
 int main(){
-    int arr[] = {-1, 0, 1, 2, -1, -4};
+    int arr[] = {0,0,0};
 
     vector<vector<int>> result = computeThreeSum(arr, sizeof(arr)/sizeof(arr[0]));
     for(int i=0; i < result.size() ; i++){
