d04e959a7d9d63b04f499aebb9e6fdcee8a9aad6
Karthik Kolathumani <karthik_kr@hotmail.com>
2021-09-21 22:29:19 -0700

    Majority Element

---
+++ Java Practice/ProgrammingPractice2021/src/LeetcodeProblems/MajorityElementVotingAlgo.java
@@ -0,0 +1,12 @@
+package LeetcodeProblems;
+import java.util.*;
+
+public class MajorityElementVotingAlgo {
+
+    public List<Integer> findMajaority(int[] nums){
+        int count = 0;
+        int majorityIndex = 0;
+        for(int i = 0 ; i < )
+    }
+
+}
