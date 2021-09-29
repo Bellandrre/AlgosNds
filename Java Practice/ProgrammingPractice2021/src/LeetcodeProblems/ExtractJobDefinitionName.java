75f2c1f83aaf657ffaa042126e017b398932948c
Karthik Kolathumani <karthik_kr@hotmail.com>
2021-09-28 22:58:49 -0700

    jlt

--- Java Practice/ProgrammingPractice2021/src/LeetcodeProblems/ExtractJobDefinitionName.java
+++ Java Practice/ProgrammingPractice2021/src/LeetcodeProblems/ExtractJobDefinitionName.java
@@ -5,6 +5,7 @@ public class ExtractJobDefinitionName {
     public static void main(String[] args) {
         String input = "JobDefinition://oracle/apps/ess/prc/pon/response/submit/SubmitResponseJob";
         System.out.println(input.substring(input.lastIndexOf('/') + 1));
-        System.out.println(input.substring(input.lastIndexOf(';')+1)); 
+        System.out.println(input.substring(input.lastIndexOf(';')+1));
     }
 }
+ 
