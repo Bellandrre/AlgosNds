e50867e64237e9f949883fc088012587f49906bc
Karthik Kolathumani <32917045+KarthikKola29@users.noreply.github.com>
2022-02-27 13:43:11 -0800

    Depth First Search

--- Java Practice/Graphs/GraphTraversals.java
+++ Java Practice/Graphs/GraphTraversals.java
@@ -45,6 +45,24 @@ public class GraphTraversals {
          }
     }
 
+    public static void depthFirstSearch(GraphNode start){
+         Set<GraphNode> seen = new HashSet<>();
+         depthFirstSearchUtil(start, new HashSet<>());
+    }
+
+    private static void depthFirstSearchUtil(GraphNode currNode, HashSet seen){
+         // Mark this node as visited
+        seen.add(currNode);
+        System.out.println(currNode.value);
+
+        for(GraphNode node : currNode.adjList){
+            if(!seen.contains(node)){
+                depthFirstSearchUtil(node, seen);
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         GraphNode starting_1 = new GraphNode();
         starting_1.value = 1;
@@ -79,8 +97,52 @@ public class GraphTraversals {
         starting_7.adjList = new ArrayList<>(Arrays.asList(starting_6, starting_8));
         starting_8.adjList = new ArrayList<>(Arrays.asList(starting_5, starting_6, starting_7));
 
-        breadthFirstSearch(starting_8);
 
+        GraphNode starting_01 = new GraphNode();
+        starting_01.value = 1;
+
+        GraphNode starting_02 = new GraphNode();
+        starting_02.value = 2;
+
+        GraphNode starting_03 = new GraphNode();
+        starting_03.value = 3;
+
+        GraphNode starting_04 = new GraphNode();
+        starting_04.value = 4;
+
+        GraphNode starting_05 = new GraphNode();
+        starting_05.value = 5;
+
+        GraphNode starting_06 = new GraphNode();
+        starting_06.value = 6;
+
+        GraphNode starting_07 = new GraphNode();
+        starting_07.value = 7;
+
+        GraphNode starting_08 = new GraphNode();
+        starting_08.value = 8;
+
+        GraphNode starting_09 = new GraphNode();
+        starting_09.value = 9;
+
+        GraphNode starting_10 = new GraphNode();
+        starting_10.value = 10;
+
+        starting_01.adjList = new ArrayList<>(Arrays.asList(starting_04, starting_02));
+        starting_02.adjList = new ArrayList<>(Arrays.asList(starting_01, starting_03, starting_05, starting_08, starting_07));
+        starting_03.adjList = new ArrayList<>(Arrays.asList(starting_04,starting_02,starting_10, starting_09));
+        starting_04.adjList = new ArrayList<>(Arrays.asList(starting_01,starting_03));
+        starting_05.adjList = new ArrayList<>(Arrays.asList(starting_02, starting_06, starting_08, starting_07));
+        starting_06.adjList = new ArrayList<>(Arrays.asList(starting_05));
+        starting_07.adjList = new ArrayList<>(Arrays.asList(starting_05, starting_02, starting_08));
+        starting_08.adjList = new ArrayList<>(Arrays.asList(starting_02, starting_05, starting_07));
+        starting_09.adjList = new ArrayList<>(Arrays.asList(starting_03));
+        starting_10.adjList = new ArrayList<>(Arrays.asList(starting_03));
+
+
+
+        //breadthFirstSearch(starting_8);
+        depthFirstSearch(starting_01);
     }
 
 
