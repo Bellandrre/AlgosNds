34632f7edfce555005f33718fd5a94f95a2ff116
BeefCat29 <32917045+BeefCat29@users.noreply.github.com>
2022-06-21 12:21:02 -0700

    More leetcode

--- Java Practice/Graphs/CountGroups.java
+++ Java Practice/Graphs/CountGroups.java
@@ -1,53 +1,52 @@
 package Graphs;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.*;
 public class CountGroups {
     // [[1,1,0],[1,1,0],[0,0,1]]
 
-    static int graph[][];
 
-    static boolean visited[][];
+    static int[][] nodesMap;
 
-    private static void constructGraph(String input[][]){
+    public static void buildGraph(String input[][]){
+        nodesMap = new int[input.length][input.length];
+
         for(int i = 0 ; i < input.length; i++){
             for(int j = 0 ; j < input[i].length; j++){
-                graph[i][j] = Integer.parseInt(input[i][j]);
-                visited[i][j] = false;
+                nodesMap[i][j] = Integer.parseInt(input[i][j]);
             }
         }
     }
 
-    public static void dfs(int[][] M, int[] visited, int i) {
-        for (int j = 0; j < M.length; j++) {
-            if (M[i][j] == 1 && visited[j] == 0) {
-                visited[j] = 1;
-                dfs(M, visited, j);
-            }
-        }
-    }
-    public static int findCircleNum() {
-        int[] visited = new int[graph.length];
+
+    public static int dfs(){
+        boolean visited[] = new boolean[nodesMap.length];
         int count = 0;
-        for (int i = 0; i < graph.length; i++) {
-            if (visited[i] == 0) {
-                dfs(graph, visited, i);
+        for(int i = 0 ; i < nodesMap.length; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                dfsUtil(visited,i);
                 count++;
             }
         }
         return count;
     }
 
+    public static void dfsUtil(boolean[] visited, int i){
+        for(int j = 0 ; j < nodesMap[i].length; j++){
+            if(nodesMap[i][j] == 1 && !visited[j]){
+                visited[j] = true;
+                dfsUtil(visited, j);
+            }
+        }
+    }
     public static void main(String[] args) {
         String input[][] = {{"1", "1", "0"},
                             {"1", "1", "0"},
                             {"0", "0", "1"}};
 
-        int rows = input.length;
-        int columns = input.length;
-
-        graph= new int[rows][columns];
-        visited = new boolean[rows][columns];
-
-        constructGraph(input);
-        System.out.println(findCircleNum());
+        buildGraph(input);
+        System.out.println(dfs());
     }
 }
---
+++ Java Practice/Graphs/PathSumII.java
@@ -0,0 +1,58 @@
+package Graphs;
+
+
+import java.util.*;
+
+public class PathSumII {
+
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val){
+            this.val = val;
+        }
+    }
+
+    public static List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> result = new ArrayList<>();
+        dfSHelper(result, new ArrayList<>(), root, targetSum, 0, new HashSet<>());
+        return result;
+    }
+
+    public static void dfSHelper(List<List<Integer>> result, List<Integer> resultSoFar, TreeNode root, int targetSum, int sumSoFar, Set<TreeNode> visited){
+        if(root == null){
+            return;
+        }
+
+        sumSoFar += root.val;
+        resultSoFar.add(root.val);
+
+        if(sumSoFar == targetSum){
+
+            result.add(new ArrayList<>(resultSoFar));
+            return;
+        }
+        
+        dfSHelper(result, resultSoFar, root.left, targetSum, sumSoFar, visited);
+            if(root.left != null) {
+               // sumSoFar -= root.left.val;
+                resultSoFar.remove(resultSoFar.size() - 1);
+            }
+        dfSHelper(result, resultSoFar, root.right, targetSum, sumSoFar, visited);
+            if(root.right != null) {
+               // sumSoFar -= root.right.val;
+                resultSoFar.remove(resultSoFar.size() - 1);
+            }
+    }
+
+    public static void main(String[] args) {
+        TreeNode newNode = new TreeNode(1);
+        newNode.left = new TreeNode(2);
+
+        System.out.println(pathSum(newNode, 1));
+
+    }
+
+}
